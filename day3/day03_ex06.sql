57224203a589852f53f4edb05d5fd85d1b081601
kaleighh <kaleighh@s21.ru>
2023-06-08 02:15:00 +0700

    03 06

--- day3/day03_ex06.sql
+++ day3/day03_ex06.sql
@@ -1,8 +1,10 @@
-WITH pizza1 AS (SELECT pi.name AS pizzeria_name_1, m.pizza_name, m.price
+WITH pizza1 AS (SELECT pi.name AS pizzeria_name_1, m.pizza_name, m.price, pizzeria_id
     FROM pizzeria pi INNER JOIN menu m ON pi.id = m.pizzeria_id),
-    pizza2 AS (SELECT pi.name AS pizzeria_name_2, m.pizza_name, m.price
+    pizza2 AS (SELECT pi.name AS pizzeria_name_2, m.pizza_name, m.price, pizzeria_id
     FROM pizzeria pi INNER JOIN menu m ON pi.id = m.pizzeria_id)
 
-SELECT *
+SELECT p1.pizza_name, pizzeria_name_1,pizzeria_name_2, p1.price
 FROM pizza1 p1
-    INNER JOIN pizza2 p2 ON p1.pizza_name = p2.pizza_name AND p1.price = 
+    INNER JOIN pizza2 p2 ON p1.pizza_name = p2.pizza_name AND p1.pizzeria_id > p2.pizzeria_id
+WHERE p1.price = p2.price
+ORDER BY 1
